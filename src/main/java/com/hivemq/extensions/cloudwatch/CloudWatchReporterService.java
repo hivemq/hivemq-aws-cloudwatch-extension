5ae24ca3e5589c253463528c48687240e3527218
LukasHiveMQ <lukas.brand@hivemq.com>
2021-07-29 14:42:34 +0200

    - Changed CloudWatch namespace to HiveMQ.

--- src/main/java/com/hivemq/extensions/cloudwatch/CloudWatchReporterService.java
+++ src/main/java/com/hivemq/extensions/cloudwatch/CloudWatchReporterService.java
@@ -41,7 +41,7 @@ class CloudWatchReporterService {
 
     private static final @NotNull Logger LOG = LoggerFactory.getLogger(CloudWatchReporterService.class);
 
-    private static final @NotNull String METRIC_NAMESPACE = "hivemq-metrics";
+    private static final @NotNull String METRIC_NAMESPACE = "HiveMQ";
 
     private @Nullable CloudWatchReporter cloudWatchReporter = null;
 
@@ -60,7 +60,7 @@ class CloudWatchReporterService {
         final Config cloudWatchConfig = configuration.getConfig();
 
         if (configuration.getEnabledMetrics().isEmpty()) {
-            LOG.warn("No hiveMQ metrics enabled, no CloudWatch report started");
+            LOG.warn("No HiveMQ metrics enabled, no CloudWatch report started");
         } else {
             final Duration apiTimeout;
             if (cloudWatchConfig.getConnectionTimeout().isPresent()) {
