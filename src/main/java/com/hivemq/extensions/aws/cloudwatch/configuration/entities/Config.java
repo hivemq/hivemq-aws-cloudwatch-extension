f36ebbcf34df3fe29cccc28cd0bae2b00ec371ff
David Sondermann <david.sondermann@hivemq.com>
2026-02-13 14:04:00 +0100

    Update JAXB dependencies to Jakarta version and refactor imports

--- src/main/java/com/hivemq/extensions/aws/cloudwatch/configuration/entities/Config.java
+++ src/main/java/com/hivemq/extensions/aws/cloudwatch/configuration/entities/Config.java
@@ -16,15 +16,15 @@
 
 package com.hivemq.extensions.aws.cloudwatch.configuration.entities;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlElementWrapper;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
