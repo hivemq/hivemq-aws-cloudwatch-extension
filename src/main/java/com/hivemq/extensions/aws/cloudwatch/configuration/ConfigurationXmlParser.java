3bedbdefab5e59f940e2dbefeeec53d9d291a502
David Sondermann <david.sondermann@hivemq.com>
2026-02-13 14:04:00 +0100

    Update JAXB dependencies to Jakarta version and refactor imports

--- src/main/java/com/hivemq/extensions/aws/cloudwatch/configuration/ConfigurationXmlParser.java
+++ src/main/java/com/hivemq/extensions/aws/cloudwatch/configuration/ConfigurationXmlParser.java
@@ -19,12 +19,12 @@ package com.hivemq.extensions.aws.cloudwatch.configuration;
 import com.hivemq.extension.sdk.api.annotations.ThreadSafe;
 import com.hivemq.extensions.aws.cloudwatch.configuration.entities.Config;
 import com.hivemq.extensions.aws.cloudwatch.configuration.entities.Metric;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import java.io.File;
 import java.io.IOException;
 
--- src/main/java/com/hivemq/extensions/aws/cloudwatch/configuration/entities/Metric.java
+++ src/main/java/com/hivemq/extensions/aws/cloudwatch/configuration/entities/Metric.java
@@ -16,14 +16,13 @@
 
 package com.hivemq.extensions.aws.cloudwatch.configuration.entities;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlValue;
 import org.jetbrains.annotations.NotNull;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.XmlValue;
 
 /**
  * @author David Sondermann
